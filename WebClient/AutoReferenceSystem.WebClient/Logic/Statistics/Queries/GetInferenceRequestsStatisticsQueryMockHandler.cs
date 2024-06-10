55359cd45d9a71bd19c2c9913a20a58433e331e5
Rukin Danil <rukin2018d@gmail.com>
2024-06-10 22:17:23 +0700

    #5 Реализация оставшихся страниц - Схемы

--- WebClient/AutoReferenceSystem.WebClient/Logic/Statistics/Queries/GetInferenceRequestsStatisticsQueryMockHandler.cs
+++ WebClient/AutoReferenceSystem.WebClient/Logic/Statistics/Queries/GetInferenceRequestsStatisticsQueryMockHandler.cs
@@ -15,33 +15,33 @@ namespace AutoReferenceSystem.WebClient.Logic.Statistics.Queries
             await Task.Yield();
             return Result.Success(new List<InferenceRequestsData>
                 {
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(1).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 125, Type = "Всего" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(1).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 100, Type = "С ошибкой" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(1).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 25, Type = "Успешно" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(1).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 25, Type = "Всего" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(1).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 23, Type = "С ошибкой" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(1).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 2, Type = "Успешно" },
 
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(2).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 9, Type = "С ошибкой" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(2).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 21, Type = "Успешно" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(2).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 30, Type = "Всего" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(5).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 9, Type = "С ошибкой" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(5).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 21, Type = "Успешно" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(5).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 30, Type = "Всего" },
 
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(3).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 48, Type = "Всего" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(3).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 40, Type = "Успешно" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(3).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 8, Type = "С ошибкой" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(10).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 15, Type = "Всего" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(10).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 14, Type = "Успешно" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(10).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 1, Type = "С ошибкой" },
 
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(4).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 46, Type = "Всего" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(4).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 10, Type = "Успешно" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(4).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 36, Type = "С ошибкой" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(15).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 40, Type = "Всего" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(15).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 40, Type = "Успешно" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(15).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 0, Type = "С ошибкой" },
 
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(5).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 35, Type = "Всего" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(5).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 35, Type = "Успешно" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(5).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 0, Type = "С ошибкой" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(20).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 35, Type = "Всего" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(20).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 35, Type = "Успешно" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(20).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 0, Type = "С ошибкой" },
 
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(6).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 87, Type = "Всего" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(6).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 87, Type = "Успешно" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(6).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 0, Type = "С ошибкой" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(25).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 27, Type = "Всего" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(25).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 23, Type = "Успешно" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(25).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 4, Type = "С ошибкой" },
 
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(7).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 53, Type = "Всего" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(7).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 50, Type = "Успешно" },
-                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(7).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 3 , Type = "С ошибкой" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(30).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 21, Type = "Всего" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(30).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 21, Type = "Успешно" },
+                    new InferenceRequestsData { CheckDate = DateTime.Now.AddMinutes(30).ToString("dd.MM.yyyy:HH:mm"), RequestsCount = 3 , Type = "С ошибкой" },
                 });
         }
     }
